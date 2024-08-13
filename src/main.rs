ec5a21f4b0a27e8d245b4f69251295e2daa22bb1
Paul Cadman <git@paulcadman.dev>
2024-08-13 13:16:57 +0100

    Initial commit of tool to generate Anoma keys

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use clap::Parser;
+use ed25519_dalek::SigningKey;
+use rand::rngs::OsRng;
+
+#[derive(Parser)]
+#[command(name = "generate-anoma-keys")]
+#[command(about = "Generate ED25519 keys for use in an Anoma/Juvix program", long_about = None)]
+struct Args {
+    /// Name of the constructor
+    #[arg(long, default_value = "mkByteArray")]
+    constructor_name: String,
+
+    /// Name of the type
+    #[arg(long, default_value = "ByteArray")]
+    type_name: String,
+}
+
+fn format_bytes_juvix(input: &[u8]) -> String {
+    let joined_string = input
+        .iter()
+        .map(|byte| format!("0x{:02x}", byte))
+        .collect::<Vec<String>>()
+        .join("; ");
+    format!("[{}]", joined_string)
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut csprng = OsRng;
+    let signing_key: SigningKey = SigningKey::generate(&mut csprng);
+
+    let keypair_bytes = signing_key.to_keypair_bytes();
+    let public_key_bytes = signing_key.verifying_key().to_bytes();
+
+    print!("privKey : {} := {} ", args.type_name, args.constructor_name);
+    print!("{}", format_bytes_juvix(&keypair_bytes));
+    println!(";");
+    print!("pubKey : {}  := {} ", args.type_name, args.constructor_name);
+    print!("{}", format_bytes_juvix(&public_key_bytes));
+    println!(";");
+}
