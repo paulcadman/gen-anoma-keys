dbbe44358e6f4de673d3359db24fa7a8afcf13dd
Paul Cadman <git@paulcadman.dev>
2024-08-20 15:39:45 +0100

    Add the option to pass a seed for the private key

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use crate::build::CLAP_LONG_VERSION;
 use clap::Parser;
-use ed25519_dalek::SigningKey;
+use ed25519_dalek::{SigningKey, SECRET_KEY_LENGTH};
 use rand::rngs::OsRng;
 use shadow_rs::shadow;
-use crate::build::CLAP_LONG_VERSION;
 
 shadow!(build);
 
@@ -11,6 +11,9 @@ shadow!(build);
 #[clap(long_version = CLAP_LONG_VERSION)]
 #[command(about = "Generate ED25519 keys for use in an Anoma/Juvix program", long_about = None)]
 struct Args {
+    /// A private key seed
+    seed: Option<String>,
+
     /// Name of the PublicKey type
     #[arg(long, default_value = "PublicKey")]
     publickey_type_name: String,
@@ -40,16 +43,31 @@ fn format_bytes_juvix(input: &[u8]) -> String {
 fn main() {
     let args = Args::parse();
 
-    let mut csprng = OsRng;
-    let signing_key: SigningKey = SigningKey::generate(&mut csprng);
+    let signing_key: SigningKey = match args.seed {
+        Some(s) => {
+            let mut decoded = [0; SECRET_KEY_LENGTH];
+            hex::decode_to_slice(s, &mut decoded).expect("Seed is not 32 bytes");
+            SigningKey::from_bytes(&decoded)
+        }
+        _ => {
+            let mut csprng = OsRng;
+            SigningKey::generate(&mut csprng)
+        }
+    };
 
     let keypair_bytes = signing_key.to_keypair_bytes();
     let public_key_bytes = signing_key.verifying_key().to_bytes();
 
-    print!("privKey : {} := {} ", args.privatekey_type_name, args.privatekey_ctor_name);
+    print!(
+        "privKey : {} := {} ",
+        args.privatekey_type_name, args.privatekey_ctor_name
+    );
     print!("{}", format_bytes_juvix(&keypair_bytes));
     println!(";");
-    print!("pubKey : {}  := {} ", args.publickey_type_name, args.publickey_ctor_name);
+    print!(
+        "pubKey : {}  := {} ",
+        args.publickey_type_name, args.publickey_ctor_name
+    );
     print!("{}", format_bytes_juvix(&public_key_bytes));
     println!(";");
 }
